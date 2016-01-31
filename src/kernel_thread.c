410f672e4aef9b2a70b0b861ecd71b7a8c2fac7c
Vikas Lokesh <vlokesh@eng.ucsd.edu>
2016-01-31 02:40:32 +0000

    kernel thread

--- src/kernel_thread.c
+++ src/kernel_thread.c
@@ -14,30 +14,30 @@ inline void pthread_function ( void *ptr ){
 } 
 int main()
 {
-int i=0;
-pthread_t thread;
-uint32_t cycles_high, cycles_low, cycles_high1, cycles_low1;
-uint64_t start, end, difference;
-for(i=0;i<OUTER_LOOP;i++){
-asm volatile ("cpuid\n\t"
-	      "rdtsc\n\t"
-	      "mov %%edx, %0\n\t"
-	      "mov %%eax, %1\n\t"
-	      : "=r" (cycles_high), "=r" (cycles_low)
-		:: "%rax", "%rbx", "%rcx", "%rdx");
-pthread_create (&thread,NULL,(void *) &pthread_function,NULL);
-pthread_join(thread, NULL);
-asm volatile ("rdtscp\n\t"
-                      "mov %%edx, %0\n\t"
-                      "mov %%eax, %1\n\t"
-                      "cpuid\n\t"
-                      : "=r" (cycles_high1), "=r" (cycles_low1)
-                        :: "%rax", "%rbx", "%rcx", "%rdx");
-}
-start = (((uint64_t)cycles_high << 32)  | cycles_low);
-end   = (((uint64_t)cycles_high1 << 32) | cycles_low1);
-difference = (end - start);
-printf("The difference is %lu - ", difference);
+	int i=0;
+	pthread_t thread;
+	uint32_t cycles_high, cycles_low, cycles_high1, cycles_low1;
+	uint64_t start, end, difference;
+	for(i=0;i<OUTER_LOOP;i++){
+		asm volatile ("cpuid\n\t"
+			      "rdtsc\n\t"
+			      "mov %%edx, %0\n\t"
+			      "mov %%eax, %1\n\t"
+			      : "=r" (cycles_high), "=r" (cycles_low)
+				:: "%rax", "%rbx", "%rcx", "%rdx");
+		pthread_create (&thread,NULL,(void *) &pthread_function,NULL);
+		pthread_join(thread, NULL);
+		asm volatile ("rdtscp\n\t"
+				      "mov %%edx, %0\n\t"
+				      "mov %%eax, %1\n\t"
+				      "cpuid\n\t"
+				      : "=r" (cycles_high1), "=r" (cycles_low1)
+				        :: "%rax", "%rbx", "%rcx", "%rdx");
+	}
+	start = (((uint64_t)cycles_high << 32)  | cycles_low);
+	end   = (((uint64_t)cycles_high1 << 32) | cycles_low1);
+	difference = (end - start);
+	printf("The difference is %lu - ", difference);
 } 
 
 

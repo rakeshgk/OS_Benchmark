29c3f8c829a6aa0b6146f02c49e3fbe9adf88e45
rakeshgk <rakeshgk1396@gmail.com>
2016-02-12 19:26:35 -0800

    Changing CPU Frequency code to repot start and end times in a file

--- src/cpu_frequency.c
+++ src/cpu_frequency.c
@@ -8,18 +8,14 @@
 #define SLEEP_TIME 10
 
 int main() {
-    double start, end, total_clocks = 0;
-    double avg_clock, stddev, variance, sum = 0;
-    double clocks[NUM_LOOP] = {0};
-    //int variable = 0;
+    uint32_t cycles_low, cycles_high;
+    uint32_t cycles_low1, cycles_high1;
+    uint64_t start, end;
     int i;
-    unsigned cycles_low, cycles_high, cycles_low1, cycles_high1;
-    printf("Loading test module... \n");
-    for (i=0; i<NUM_LOOP; i++) {
-        printf("Iteration # %d \n", i);
-        //preempt_disable();            /*we disable preemption on our CPU*/
-        //raw_local_irq_save(flags);    /*we disable hard interrupts on our CPU*/
+    FILE* fp;
+    fp = fopen("../data/cpu_frequency.csv", "w");
 
+    for (i=0; i<NUM_LOOP; i++) {
         asm volatile (
             "CPUID\n\t"
             "RDTSC\n\t"
@@ -37,26 +33,10 @@ int main() {
             "CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1):: "%rax",
             "%rbx", "%rcx", "%rdx");
 
-        //raw_local_irq_restore(flags);     /*we enable hard interrupts on our CPU*/
-        //preempt_enable(); /*we enable preemption*/
-
-        start = (double) ( ((uint64_t)cycles_high << 32) | cycles_low );
-        end = (double) ( ((uint64_t)cycles_high1 << 32) | cycles_low1 );
-
-        printf("\n function execution time is %f clock cycles \n", (end - start));
-        clocks[i] = (end - start);
-        total_clocks += clocks[i];
+        start = (((uint64_t) cycles_high << 32) | cycles_low);
+        end   = (((uint64_t) cycles_high1 << 32) | cycles_low1);
+        fprintf(fp, "%ld,%ld\n", start, end);
     }
-
-    avg_clock = total_clocks/NUM_LOOP;
-    for (i=0; i<NUM_LOOP; i++) {
-        sum += pow((clocks[i] - avg_clock), 2);
-    }
-
-    variance = sum/(NUM_LOOP - 1);
-        stddev = sqrt(variance);
-    printf("average clock cycles for time is %f \n", avg_clock);
-    printf("variance is %f \n", variance);
-    printf("standard deviation is %f \n", stddev);
+    fclose(fp);
     return 0;
 }

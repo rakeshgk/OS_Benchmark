5f74180545cd3e98fde6635922c7e1a8df8dacf8
rakeshgk <rakeshgk1396@gmail.com>
2016-02-12 20:48:10 -0800

    Changing system_call_overhead_0 to print start and end times to a file

--- src/system_call_overhead_0.c
+++ src/system_call_overhead_0.c
@@ -11,25 +11,23 @@
 #define NUM_LOOP 1000
 
 int main() {
-    double start = 0, end = 0, total_clocks = 0;
-    double avg_clock, stddev, variance, sum = 0;
-    double clocks[NUM_LOOP] = {0};
-    //int variable = 0;
+    uint32_t cycles_low, cycles_high;
+    uint32_t cycles_low1, cycles_high1;
+    uint64_t start, end;
     int i;
-    unsigned cycles_low, cycles_high, cycles_low1, cycles_high1;
-    pid_t child_pid, tpid, process_pid;
+    pid_t child_pid, process_pid;
     int child_status;
-    printf("Loading test module... \n");
+
+    FILE* fp;
+    fp = fopen("../data/system_call_overhead_0.csv", "w");
+
     for (i=0; i<NUM_LOOP; i++) {
-        //preempt_disable();            /*we disable preemption on our CPU*/
-        //raw_local_irq_save(flags);    /*we disable hard interrupts on our CPU*/
-
-        // perform the actual operation
-        //measured_function();
-
         child_pid = fork();
         if(child_pid == 0) {
-            /* This is done by the child process. */
+            /*
+                We take the PID value from within the child process.
+                This is done to ensure that the PID values are not cached
+            */
             asm volatile (
                 "CPUID\n\t"
                 "RDTSC\n\t"
@@ -46,35 +44,19 @@ int main() {
                 "CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1):: "%rax",
                 "%rbx", "%rcx", "%rdx");
 
-            start = (double) ( ((uint64_t)cycles_high << 32) | cycles_low );
-            end = (double) ( ((uint64_t)cycles_high1 << 32) | cycles_low1 );
-            printf("\n Iteration %d, time: %f\n", i, (end - start));
-
+            start = (((uint64_t)cycles_high << 32) | cycles_low);
+            end   = (((uint64_t)cycles_high1 << 32) | cycles_low1);
+            fprintf(fp, "%ld,%ld\n", start, end);
             exit(0);
-        } else {
-            /* This is run by the parent.  Wait for the child to terminate. */
-            //do {
-            //    tpid = wait(&child_status);
-            //} while(tpid != child_pid);
+        }
+        else {
+            /* This is run by the parent.
+               The parent does nothing apart from waiting for the child
+            */
             waitpid(child_pid, &child_status, 0);
         }
-
-        //raw_local_irq_restore(flags);     /*we enable hard interrupts on our CPU*/
-        //preempt_enable(); /*we enable preemption*/
-
-        clocks[i] = (end - start);
-        total_clocks += clocks[i];
     }
 
-    avg_clock = total_clocks/NUM_LOOP;
-    for (i=0; i<NUM_LOOP; i++) {
-        sum += pow((clocks[i] - avg_clock), 2);
-    }
-
-    variance = sum/(NUM_LOOP - 1);
-        stddev = sqrt(variance);
-    printf("average clock cycles for time is %f \n", avg_clock);
-    printf("variance is %f \n", variance);
-    printf("standard deviation is %f \n", stddev);
+    fclose(fp);
     return 0;
 }

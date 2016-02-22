6f1b2ebba3cc9e1b40ac1501d158624eda0ff0a3
rakeshgk <rakeshgk1396@gmail.com>
2016-02-22 02:14:34 -0800

    Adding modified task creation times 1 file

--- src/task_creation_1.c
+++ src/task_creation_1.c
@@ -25,35 +25,33 @@ int main() {
     uint32_t cycles_low, cycles_high, cycles_low1, cycles_high1;
     int i;
     FILE* fp;
-    fp = fopen("../data/task_creation_1.csv", "w");
+    fp = fopen("../data/task_creation_1.csv", "a");
 
-    for (i=0; i<NUM_LOOP; i++) {
-        pthread_t thread;
+    pthread_t thread;
 
-        asm volatile (
-            "CPUID\n\t"
-            "RDTSC\n\t"
-            "mov %%edx, %0\n\t"
-            "mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low)::
-            "%rax", "%rbx", "%rcx", "%rdx");
+    asm volatile (
+       "CPUID\n\t"
+       "RDTSC\n\t"
+       "mov %%edx, %0\n\t"
+       "mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low)::
+       "%rax", "%rbx", "%rcx", "%rdx");
 
-        // perform the actual operation
-        pthread_create(&thread, NULL, (void *)&pthread_function, NULL);
+    // perform the actual operation
+    pthread_create(&thread, NULL, (void *)&pthread_function, NULL);
 
-        asm volatile(
-            "RDTSCP\n\t"
-            "mov %%edx, %0\n\t"
-            "mov %%eax, %1\n\t"
-            "CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1):: "%rax",
-            "%rbx", "%rcx", "%rdx");
+    asm volatile(
+       "RDTSCP\n\t"
+       "mov %%edx, %0\n\t"
+       "mov %%eax, %1\n\t"
+       "CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1):: "%rax",
+       "%rbx", "%rcx", "%rdx");
 
 
-        start = (((uint64_t)cycles_high << 32) | cycles_low);
-        end   = (((uint64_t)cycles_high1 << 32) | cycles_low1);
-        fprintf(fp, "%lu,%lu\n", start, end);
+    start = (((uint64_t)cycles_high << 32) | cycles_low);
+    end   = (((uint64_t)cycles_high1 << 32) | cycles_low1);
+    fprintf(fp, "%lu,%lu\n", start, end);
 
-        pthread_join(thread, NULL);
-    }
+    pthread_join(thread, NULL);
     fclose(fp);
     return 0;
 }

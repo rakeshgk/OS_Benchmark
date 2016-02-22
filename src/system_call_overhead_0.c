16118962b673074cb4508b9e5ae3af3794abba24
bdaemon <amit.borase@yahoo.in>
2016-02-21 23:30:04 -0800

    corrected system call overhead

--- src/system_call_overhead_0.c
+++ src/system_call_overhead_0.c
@@ -8,54 +8,39 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define NUM_LOOP 1000
-
 int main() {
     uint32_t cycles_low, cycles_high;
     uint32_t cycles_low1, cycles_high1;
     uint64_t start, end;
-    int i;
-    pid_t child_pid, process_pid;
-    int child_status;
+    pid_t process_pid;
 
     FILE* fp;
-    fp = fopen("../data/system_call_overhead_0.csv", "w");
+    fp = fopen("../data/system_call_overhead_0.csv", "a");
 
-    for (i=0; i<NUM_LOOP; i++) {
-        child_pid = fork();
-        if(child_pid == 0) {
-            /*
-                We take the PID value from within the child process.
-                This is done to ensure that the PID values are not cached
-            */
-            asm volatile (
+    /*
+     We take the PID value from within the child process.
+     This is done to ensure that the PID values are not cached
+     */
+    asm volatile (
                 "CPUID\n\t"
                 "RDTSC\n\t"
                 "mov %%edx, %0\n\t"
                 "mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low)::
                 "%rax", "%rbx", "%rcx", "%rdx");
 
-            process_pid = getpid();
+    process_pid = getpid();
 
-            asm volatile(
+    asm volatile(
                 "RDTSCP\n\t"
                 "mov %%edx, %0\n\t"
                 "mov %%eax, %1\n\t"
                 "CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1):: "%rax",
                 "%rbx", "%rcx", "%rdx");
 
-            start = (((uint64_t)cycles_high << 32) | cycles_low);
-            end   = (((uint64_t)cycles_high1 << 32) | cycles_low1);
-            fprintf(fp, "%lu,%lu\n", start, end);
-            exit(0);
-        }
-        else {
-            /* This is run by the parent.
-               The parent does nothing apart from waiting for the child
-            */
-            waitpid(child_pid, &child_status, 0);
-        }
-    }
+    start = (((uint64_t)cycles_high << 32) | cycles_low);
+    end   = (((uint64_t)cycles_high1 << 32) | cycles_low1);
+    fprintf(fp, "%lu,%lu\n", start, end);
+   
     fclose(fp);
     return 0;
 }

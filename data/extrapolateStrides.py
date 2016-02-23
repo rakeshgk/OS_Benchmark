81ecea32fd68706666f4ff2ead0e154c375e0851
bdaemon <amit.borase@yahoo.in>
2016-02-22 23:32:13 -0800

    update

--- data/extrapolateStrides.py
+++ data/extrapolateStrides.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-
+import pprint
 def main():
 	csvFile="memory_latency_0.csv"
 	flag=False
@@ -12,35 +12,60 @@ def main():
 		content = f.readlines()
 		cycles=[]
 		for entry in content:
-			sample=int(entry.split(",")[0])
-			size=int(entry.split(",")[1])
-			stride=int(entry.split(",")[2])
-			cycle=int(entry.split(",")[3])
-			if sample==0 and len(cycles)>0:
+			if entry=='\n':
 				cycles=np.array(cycles)				
 				cycles=	removeOutliers(cycles)			
 				avg=mean(cycles)
 				stdev=sdDeviation(cycles)
 				output.append((avg,stdev,size,stride))
-				cycles=[]		
+				cycles=[]
+				#cycles.append(cycle)		
 				
 			else:
-				flag=True				
+				sample=int(entry.split(",")[0])
+				size=int(entry.split(",")[1])
+				stride=int(entry.split(",")[2])
+				cycle=int(entry.split(",")[3])
+				#flag=True				
 				cycles.append(cycle)
-	print output
 	xaxis=[]
 	yaxis=[]
+	outstride={}
+	for e in outputs:
+		outstride[e]=[]
 	for e in outputs:
 		x=[]
 		y=[]
 		for entry in output:
-			strides=entry[-1]
-			if strides==e:
-				x.append(math.log(entry[-2],2))
-				y.append(entry[0]*0.41666*(10**(-9)))
+			stride=entry[-1]
+			size=entry[-2]
+			avg=entry[0]
+			stddev=entry[1]	
+			if stride==e:
+				if size>stride:
+					outstride[e].append((size,avg,stddev))
+					x.append(math.log(entry[-2],2))
+					y.append(entry[0]*0.41666*(10**(-9)))
 		xaxis.append(x)
 		yaxis.append(y)
+	 
 	i=0
+	pp = pprint.PrettyPrinter(depth=6)
+	for e in outputs:
+		if e==512:
+			start=1024
+		elif e==1024:
+			start=2048
+		else:
+			start=512
+		print e
+		filename=str(e)+".csv"
+		f = open(filename,'w')
+		for elem in outstride[e]:
+			if elem[0]==start: 
+				f.write(str(elem[0]) + " " + str(elem[1]) + " " + str(elem[2]) + "\n")	
+				start*=2
+	
 	while i<len(xaxis):
 		plt.plot(xaxis[i],yaxis[i], label=str(outputs[i]))
 		i+=1

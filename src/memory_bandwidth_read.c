8b7ab0c8c1fc9de75e0011f0f482d0652a696b57
Vikas Lokesh <vlokesh@eng.ucsd.edu>
2016-03-04 03:20:36 +0000

    memory bandwidth corrected

--- src/memory_bandwidth_read.c
+++ src/memory_bandwidth_read.c
@@ -10,18 +10,19 @@
 #include <sys/wait.h>
 
 #define NUM_LOOP 1000
-#define CACHE_SIZE 4736000
-#define MEM_SIZE 9472000
+#define CACHE_LINE_SIZE 256
+#define ARRAY_SIZE 100000000
 
 int main() {
     uint64_t start, end;
-    int i,j,temp,counter;
-    int *array;
+    int i,j,counter;
+    char temp;
+    char *array;
     unsigned cycles_low, cycles_high, cycles_low1, cycles_high1;
     FILE* fp;
     fp = fopen("../data/memory_bandwidth_read.csv", "w");
-    array=(int *)malloc(MEM_SIZE);
-    counter=MEM_SIZE/sizeof(int);
+    array=(char *)malloc(ARRAY_SIZE);
+    counter=ARRAY_SIZE;
     for (i=0; i<NUM_LOOP; i++) {
 	asm volatile ("cpuid\n\t"
 		          "rdtsc\n\t"
@@ -30,7 +31,7 @@ int main() {
 		          : "=r" (cycles_high), "=r" (cycles_low)
 		           :: "%rax", "%rbx", "%rcx", "%rdx");
 	start = ( ((uint64_t)cycles_high << 32) | cycles_low );
-	for(j=0;j<counter;j++){
+	for(j=0;j<counter;j+=CACHE_LINE_SIZE){
 		temp=array[j];
 	}
 
